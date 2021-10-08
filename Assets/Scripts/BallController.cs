be53a9913f3ef91306cd7b0e1df7913ff6527de1
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-09 00:47:42 +0700

    bola bisa bergerak dengan WSAD

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,18 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class BallController : MonoBehaviour
 {
-    
-    Scene scene;
+    // 1-2
     public float xInitialForce;
     public float yInitialForce;
+
     private Rigidbody2D rigidBody2D;
 
+    string curScene;
+
+    // WASD move keys
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode rightButton = KeyCode.D;
+    public KeyCode leftButton = KeyCode.A;
+    public float speed = 19.0f;
 
     // Start is called before the first frame update
     private void Start()
     {       
         Scene scene = SceneManager.GetActiveScene();
-        string curScene = scene.name;
+        curScene = scene.name;
 
         // atur behavior bola tergantung jenis scene
         switch (curScene)
@@ -33,8 +41,54 @@ public class BallController : MonoBehaviour
                 yeetBallRandomly();
                 break;
             case "4":
+                rigidBody2D = GetComponent<Rigidbody2D>();
                 break;
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // atur behavior bola tergantung jenis scene
+        switch (curScene)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                controlBall();
+                break;
+        }
+    }
+
+    private void controlBall()
+    {
+        Vector2 velocity = rigidBody2D.velocity;
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else if (Input.GetKey(rightButton))
+        {
+            velocity.x = speed;
+        }
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+            velocity.x = 0.0f;
+        }
+        rigidBody2D.velocity = velocity;
     }
 
     private void yeetBallRandomly()
@@ -60,9 +114,5 @@ public class BallController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
