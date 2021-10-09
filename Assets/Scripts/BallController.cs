9090f7da5d2d10169c6563a238484cc2538f47b1
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-09 10:21:53 +0700

    merapikan kode

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -13,13 +13,7 @@ public class BallController : MonoBehaviour
     // universal
     private Rigidbody2D rigidBody2D;
     string curScene;
-
-    // 4, WASD move keys
-    KeyCode upButton = KeyCode.W;
-    KeyCode downButton = KeyCode.S;
-    KeyCode rightButton = KeyCode.D;
-    KeyCode leftButton = KeyCode.A;
-    float speed = 19.0f;
+    
 
     // Start is called before the first frame update
     private void Start()
@@ -38,52 +32,12 @@ public class BallController : MonoBehaviour
                 rigidBody2D = GetComponent<Rigidbody2D>();
                 yeetBallRandomly();
                 break;
-            case "4":
-                rigidBody2D = GetComponent<Rigidbody2D>();
-                break;
-            case "5":
-                rigidBody2D = GetComponent<Rigidbody2D>();
-                break;
         }
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // atur behavior bola tergantung jenis scene
-        switch (curScene)
-        {
-            case "4":
-                controlBall();
-                break;
-        }
-    }
-
-    private void controlBall()
-    {
-        Vector2 velocity = rigidBody2D.velocity;
-        if (Input.GetKey(upButton))
-        {
-            velocity.y = speed;
-        }
-        else if (Input.GetKey(downButton))
-        {
-            velocity.y = -speed;
-        }
-        else if (Input.GetKey(rightButton))
-        {
-            velocity.x = speed;
-        }
-        else if (Input.GetKey(leftButton))
-        {
-            velocity.x = -speed;
-        }
-        else
-        {
-            velocity.y = 0.0f;
-            velocity.x = 0.0f;
-        }
-        rigidBody2D.velocity = velocity;
     }
 
     private void yeetBallRandomly()
---
+++ Assets/Scripts/WSADControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WSADControl : MonoBehaviour
+{
+    // WASD move keys
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode rightButton = KeyCode.D;
+    public KeyCode leftButton = KeyCode.A;
+    public float speed = 19.0f;
+
+    private Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = rigidBody2D.velocity;
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else if (Input.GetKey(rightButton))
+        {
+            velocity.x = speed;
+        }
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+            velocity.x = 0.0f;
+        }
+        rigidBody2D.velocity = velocity;
+    }
+}
