d5a5aa68d6c80feb258cbc54ff771f0592987514
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-09 10:15:09 +0700

    memisah class untuk mengontrol bola

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -14,12 +14,7 @@ public class BallController : MonoBehaviour
 
     string curScene;
 
-    // WASD move keys
-    public KeyCode upButton = KeyCode.W;
-    public KeyCode downButton = KeyCode.S;
-    public KeyCode rightButton = KeyCode.D;
-    public KeyCode leftButton = KeyCode.A;
-    public float speed = 19.0f;
+    
 
     // Start is called before the first frame update
     private void Start()
@@ -30,8 +25,6 @@ public class BallController : MonoBehaviour
         // atur behavior bola tergantung jenis scene
         switch (curScene)
         {
-            case "1":
-                break;
             case "2":
                 rigidBody2D = GetComponent<Rigidbody2D>();
                 yeetBallRandomly();
@@ -39,9 +32,6 @@ public class BallController : MonoBehaviour
             case "3":
                 rigidBody2D = GetComponent<Rigidbody2D>();
                 yeetBallRandomly();
-                break;
-            case "4":
-                rigidBody2D = GetComponent<Rigidbody2D>();
                 break;
         }
     }
@@ -49,46 +39,7 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // atur behavior bola tergantung jenis scene
-        switch (curScene)
-        {
-            case "1":
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-            case "4":
-                controlBall();
-                break;
-        }
-    }
-
-    private void controlBall()
-    {
-        Vector2 velocity = rigidBody2D.velocity;
-        if (Input.GetKey(upButton))
-        {
-            velocity.y = speed;
-        }
-        else if (Input.GetKey(downButton))
-        {
-            velocity.y = -speed;
-        }
-        else if (Input.GetKey(rightButton))
-        {
-            velocity.x = speed;
-        }
-        else if (Input.GetKey(leftButton))
-        {
-            velocity.x = -speed;
-        }
-        else
-        {
-            velocity.y = 0.0f;
-            velocity.x = 0.0f;
-        }
-        rigidBody2D.velocity = velocity;
+        
     }
 
     private void yeetBallRandomly()
