e1fd26992916f787a893ff091a85a5cca37177e0
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-09 12:46:43 +0700

    box sudah hilang jika ditabrak dan akan menambah skor

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    GameObject player;
+
+    void Awake()
+    {
+        // get gameobject player
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // callback jika ada yang masuk trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player && other.isTrigger == false)
+        {
+            Destroy(this.gameObject);
+            ScoreManager.score++;
+        }
+    }
+}
