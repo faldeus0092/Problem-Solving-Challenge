69d20d42f7bc24999373f9721d9563ea8dfaa0cb
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-09 10:28:22 +0700

    cleanup code

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -14,16 +14,10 @@ public class BallController : MonoBehaviour
     private Rigidbody2D rigidBody2D;
     string curScene;
 
-    // 4, WASD move keys
-    KeyCode upButton = KeyCode.W;
-    KeyCode downButton = KeyCode.S;
-    KeyCode rightButton = KeyCode.D;
-    KeyCode leftButton = KeyCode.A;
-    float speed = 19.0f;
 
     // Start is called before the first frame update
     private void Start()
-    {       
+    {
         Scene scene = SceneManager.GetActiveScene();
         curScene = scene.name;
 
@@ -38,52 +32,12 @@ public class BallController : MonoBehaviour
                 rigidBody2D = GetComponent<Rigidbody2D>();
                 yeetBallRandomly();
                 break;
-            case "4":
-                rigidBody2D = GetComponent<Rigidbody2D>();
-                break;
-            case "5":
-                rigidBody2D = GetComponent<Rigidbody2D>();
-                break;
         }
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // atur behavior bola tergantung jenis scene
-        switch (curScene)
-        {
-            case "4":
-                controlBall();
-                break;
-        }
-    }
-
-    private void controlBall()
-    {
-        Vector2 velocity = rigidBody2D.velocity;
-        if (Input.GetKey(upButton))
-        {
-            velocity.y = speed;
-        }
-        else if (Input.GetKey(downButton))
-        {
-            velocity.y = -speed;
-        }
-        else if (Input.GetKey(rightButton))
-        {
-            velocity.x = speed;
-        }
-        else if (Input.GetKey(leftButton))
-        {
-            velocity.x = -speed;
-        }
-        else
-        {
-            velocity.y = 0.0f;
-            velocity.x = 0.0f;
-        }
-        rigidBody2D.velocity = velocity;
     }
 
     private void yeetBallRandomly()
@@ -94,20 +48,20 @@ public class BallController : MonoBehaviour
 
         // Arah kiri/kanan secara random
         float randomDirection = UnityEngine.Random.Range(0, 2);
-        
+
         // Menggunakan Phytagoras agar kecepatannya konstan
         float ballSpeed = yInitialForce * yInitialForce;
         xInitialForce = Mathf.Sqrt(ballSpeed - (yRandomInitialForce * yRandomInitialForce));
-        
+
         if (randomDirection < 1.0f)
         {
             rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
         }
-        else 
+        else
         {
             rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
         }
     }
 
-    
+
 }
