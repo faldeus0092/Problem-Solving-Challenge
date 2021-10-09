88437f1f50b38238641256540448c7b36f76a013
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-09 11:54:42 +0700

    menggenerate kotak secara random baik posisi maupun jumlah

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject boxPrefab = null;
+    public float minX, minY, maxX, maxY;
+    public int minQty, maxQty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int random = Random.Range(minQty, maxQty);
+        while (random != 0)
+        {
+            Instantiate(boxPrefab, new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY)), Quaternion.identity);
+            random--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
